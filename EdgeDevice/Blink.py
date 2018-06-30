f94604a03dec1de4141fb0cf8b2c5cdb624d007e
Pablo Avila <jose.9526@gmail.com>
2018-06-30 16:55:43 -0600

    Add instructions to install and use RFID code, control 2 leds on Blink.py

--- EdgeDevice/Blink.py
+++ EdgeDevice/Blink.py
@@ -13,27 +13,35 @@ import RPi.GPIO as GPIO
 import time
 
 #-------------------- Constants and variables
-RedLed = 11    # pin11
+RedLed = 11    # pin11 - GPIO 17
+GreenLed = 29  # pin29 - GPIO 5
 
 #-------------------- Setup
 def setup():
-    GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-    GPIO.setup(RedLed, GPIO.OUT)   # Set RedLed's mode is output
-    GPIO.output(RedLed, GPIO.LOW) # Set RedLed high(+3.3V) to turn on led
+    GPIO.setmode(GPIO.BOARD)            # Numbers GPIOs by physical location
+    # Red Led
+    GPIO.setup(RedLed, GPIO.OUT)        # Set RedLed's mode is output
+    GPIO.output(RedLed, GPIO.LOW)       # Set RedLed high(+3.3V) to turn on led
+    # Green Led
+    GPIO.setup(GreenLed, GPIO.OUT)      # Set GreenLed's mode is output
+    GPIO.output(GreenLed, GPIO.LOW)     # Set GreenLed high(+3.3V) to turn on led
 
 #-------------------- Methods
 def blink():
-    GPIO.output(RedLed, GPIO.HIGH)  # led on
+    GPIO.output(RedLed, GPIO.HIGH)      # Red led on
+    GPIO.output(GreenLed, GPIO.LOW)     # Green led off
     time.sleep(1)
-    GPIO.output(RedLed, GPIO.LOW) # led off
+    GPIO.output(RedLed, GPIO.LOW)       # Red led off
+    GPIO.output(GreenLed, GPIO.HIGH)    # Green led on
     time.sleep(1)
 
 def destroy():
-    GPIO.output(RedLed, GPIO.LOW)   # led off
-    GPIO.cleanup()                  # Release resource
+    GPIO.output(RedLed, GPIO.LOW)       # Red led off
+    GPIO.output(GreenLed, GPIO.LOW)     # Green led off
+    GPIO.cleanup()                      # Release resource
 
 ##################### MAIN
-if __name__ == '__main__':     # Program start from here
+if __name__ == '__main__':              # Program start from here
     setup()
 
     #-------------------- Main Loop
@@ -42,5 +50,6 @@ if __name__ == '__main__':     # Program start from here
             blink()
 
     #-------------------- Exceptions
-    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+    # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+    except KeyboardInterrupt:
         destroy()
